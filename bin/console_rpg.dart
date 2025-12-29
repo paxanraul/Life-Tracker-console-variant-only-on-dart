857cf442751de468dcaa99d0941b44ea4faf6d96
Raul <raul20081033@gmail.com>
2025-12-29 13:17:27 +0300

    Консольная залупа

---
+++ bin/console_rpg.dart
@@ -0,0 +1,49 @@
+import "dart:io";
+import "dart:convert";  // ← ВАЖНО!
+
+void main(){
+  
+  stdout.encoding = utf8;
+  
+  int exp = 0;
+  int gold = 100; 
+  int level = 1;
+
+  stdout.write('Привет! Назови своего персонажа: ');
+  
+ 
+  String salam = stdin.readLineSync(encoding: utf8)!.trim(); 
+  
+  print('Отлично, приятно познакомиться $salam!\n');
+  
+  print("📊 ВАША СТАТИСТИКА:");
+  print("Опыт: $exp");
+  print("Золото: $gold");
+  print("Уровень: $level\n");
+  
+  print("Не желаете ли приобрести начальный пакет для быстрого результата?");
+  stdout.write("(да/нет): ");
+  
+
+  String answer = stdin.readLineSync(encoding: utf8)!.trim().toLowerCase();
+  
+  
+  if (answer == "да") {
+    print("\n✅ Вы выбрали: ДА");
+    stdout.write("Отлично, выберите способ оплаты (сбп/карта): ");
+    
+    String payment = stdin.readLineSync(encoding: utf8)!.trim();
+    print("Вы выбрали оплату через $payment. Спасибо за покупку!");
+    
+  } else if (answer == "нет") {
+    print("\n❌ Вы выбрали: НЕТ");
+    stdout.write("\nПочему нет? Напишите об этом пожалуйста: ");
+    String explanation = stdin.readLineSync(encoding: utf8)!.trim();
+    stdout.write("\nВаш ответ: $explanation");
+    print("\nСпасибо за обратную связь, хорошей игры!");
+    
+  } else {
+    print("\n🤔 Непонятный ответ: '$answer'");
+    print("\nИгра продолжается в обычном режиме...");
+  }
+}
---
+++ bin/test.dart
@@ -0,0 +1,8 @@
+
+void main(List<String> arguments) {
+int? r = null;
+r = 5;
+
+var v = r + 5;
+print(v);
+}
